3bceb8d1ed5c377dc58a171671d796725edc5004
Aurelio Chagas <aurelio.chagas.c@gmail.com>
2023-11-22 20:56:55 -0300

    Projeto final Tailwind CSS

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
+
+export function Profile() {
+  return (
+    <div className="grid grid-cols-profile items-center gap-3">
+      <img
+        src="https://github.com/Aurelio-ch.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Diego Fernandes
+        </span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          diego@rocketseat.com.br
+        </span>
+      </div>
+      <Button type="button" variant="ghost">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </Button>
+    </div>
+  )
+}
